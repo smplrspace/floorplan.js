e692ade7f995154cf7f92ee6c6efb5734c8f69a4
Thibaut Tiberghien <thibaut@smplrspace.com>
2022-02-07 13:19:16 +0800

    npm package post close sourcing

---
+++ index.js
@@ -0,0 +1,8 @@
+const notice = `
+  This repo was close sourced.
+  Visit https://www.smplrspace.com to learn more about 
+  our commercial tooling for adding floor plans to your 
+  app as well as our Javascript library.
+`
+
+export default notice
